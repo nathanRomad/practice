72e4c8321690796329bf63acaf20038cb1288bf3
nathanRomad <nathan.a.hamilton@gmail.com>
2021-02-10 16:17:48 -0600

    part1 and 2 complete.

--- main.js
+++ main.js
@@ -57,17 +57,17 @@ const tenAlbum = {
 
 // Dot vs. Bracket Notation - accessing properties
 // TODO: USING DOT NOTATION, access the object properties (artist, title, released) and log the following message to the console using string interpolation: "Pearl Jam's Album Ten was released on 8/27/1991"
-console.log("Artist:", tenAlbum.artist, "Title:", tenAlbum.title, "Release Date", tenAlbum.released)
+// console.log("Artist:", tenAlbum.artist, "Title:", tenAlbum.title, "Release Date", tenAlbum.released)
 
 // TODO: Output the same messge to the console using BRACKET NOTATION
-console.log(tenAlbum["artist"], "'s", tenAlbum["title"], "was released on", tenAlbum["released"])
+// console.log(tenAlbum["artist"], "'s", tenAlbum["title"], "was released on", tenAlbum["released"])
 
 // Iterating Object keys/values
 // TODO: Iterate the tenAlbum object and log the key/value pairs to the console using a for/in loop
 // example: "title": "Ten"
-for (const item in tenAlbum) {
-    console.log(`${item}: ${tenAlbum[item]}`)
-}
+// for (const item in tenAlbum) {
+//     console.log(`${item}: ${tenAlbum[item]}`)
+// }
 
 // Accessing iterable properties on objects
 // TODO: Iterate the tracks array and output the title of each song and its song length using a for loop of your choosing. The output should look something like this:
@@ -76,36 +76,38 @@ for (const item in tenAlbum) {
     Title: Even Flow - Length: 293
     etc.
 */
-for (const object of tenAlbum.tracks) {
-    console.log(`Title: ${object.title} - Length: ${object.length}`)
-}
+// for (const object of tenAlbum.tracks) {
+//     console.log(`Title: ${object.title} - Length: ${object.length}`)
+// }
 // ! ADD AND COMMIT
 
 // OBJECT.keys() / .values() / .entries()
 // TODO: log the keys of the tenAlbum object using Object.keys()
 // Object.keys() = Returns an array containing the names of all of the given object's own enumerable string properties.
-console.log(Object.keys(tenAlbum))
+// console.log(Object.keys(tenAlbum))
 
 // TODO: log the values of the tenAlbum object Object.values()
 // Object.values() = Returns an array containing the values that correspond to all of a given object's own enumerable string properties.
-console.log(Object.values(tenAlbum))
+// console.log(Object.values(tenAlbum))
 
 // TODO: log the entries of the tenAlbum object Object.entries()
 // Object.entries() = Returns an array containing all of the [key, value] pairs of a given object's own enumerable string properties.
-console.log(Object.entries(tenAlbum))
+// console.log(Object.entries(tenAlbum))
 
 // TODO: iterate the tenAlbum object and log the key/value pairs to the console using each of the above Object methods:
 // .keys()
-for (let key of Object.keys(tenAlbum)) {
-    console.log(key, tenAlbum[key])
-}
+// for (let key of Object.keys(tenAlbum)) {
+//     console.log(key, tenAlbum[key])
+// }
 // .values()
-for (let value of Object.values(tenAlbum)) {
-    const key = Object.keys(tenAlbum).find(key => tenAlbum[key] === value)
-    console.log(key, value);
-}
+// for (let value of Object.values(tenAlbum)) {
+//     const key = Object.keys(tenAlbum).find(key => tenAlbum[key] === value)
+//     console.log(key, value);
+// }
 // .entries()
-
+// for (const [key, value] of Object.entries(tenAlbum)) {
+//     console.log(`Key: ${key} - Value: ${value}`);
+// }
 // ! ADD AND COMMIT
 
 // USING THE DATA FROM .setlists
